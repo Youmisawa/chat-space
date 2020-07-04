20ed3ce6e6bd42395c0be481c8a88b9e26093a42
Youmisawa <66550111+Youmisawa@users.noreply.github.com>
2020-07-04 20:56:52 +0900

    receive the returned JSON with the done method and create HTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,46 @@
 $(function () {
+  function buildHTML(message) {
+    if (message.image) {
+      let html =
+        `<div class="Message-list">
+          <div class="Message-info">
+            <div class="Message-info__user-name">
+              ${message.user_name}
+            </div>
+            <div class="Message-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.body}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="Message-list">
+        <div class="Message-info">
+          <div class="Message-info__user-name">
+            ${message.user_name}
+          </div>
+          <div class="Message-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Message">
+          <p class="Message__content">
+            ${message.body}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+
   $('.Form').on('submit', function (e) {
     e.preventDefault()
     let formData = new FormData(this);
@@ -10,6 +52,9 @@ $(function () {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function (data) {
+      let html = buildHTML(data);
     })
   });
 });
