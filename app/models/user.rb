2bdf4c65c4592529b46402b1e53e46c7fd82f35c
Youmisawa <66550111+Youmisawa@users.noreply.github.com>
2020-07-05 17:05:46 +0900

    asynchronous communication at event

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def self.search(input, id)
+    return nil if input == ""
+    User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+  end
+  
   validates :name, presence: true, uniqueness: true
   has_many :groups_users
   has_many :groups, through: :groups_users
